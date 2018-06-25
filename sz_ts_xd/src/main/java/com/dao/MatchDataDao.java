335821e886cf5e0a0bc11340412a75dc7f9c25c2
lujun <13226517469@163.com>
2018-06-25 14:30:09 +0800

    搭配款式数据采集dao层

---
+++ sz_ts_xd/src/main/java/com/dao/MatchDataDao.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import com.model.MatchData;
+
+/**
+ * @author lujun
+ * @date 2018年6月25日
+ */
+public interface MatchDataDao {
+	//插入搭配数据
+	int insertMatchData(MatchData matchData);
+}
