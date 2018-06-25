21570786a4258ed84ea7bb740fcce36e9c3453c6
lujun <13226517469@163.com>
2018-06-25 14:31:22 +0800

    款式数据采集dao层

---
+++ sz_ts_xd/src/main/java/com/dao/TryOnDataDao.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import com.model.TryOnData;
+
+/**
+ * @author lujun
+ * @date 2018年6月25日
+ */
+public interface TryOnDataDao {
+	//插入试穿数据
+	int insertTryOnData(TryOnData tryOnData);
+}
